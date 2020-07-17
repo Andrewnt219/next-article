215fed5bb5e1314ec2fa858d73abe8288c38e946
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-07-17 19:03:04 -0400

    added SocialMedia

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,12 +1,32 @@
 import React, { ReactElement } from "react";
 import styled from "styled-components";
+import { Logo } from "../ui/Logo";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import { SocialMedia } from "../ui/SocialMedia";
 
 type Props = {
   height: string;
 };
 
 function Footer({ height }: Props): ReactElement {
-  return <Container style={{ height }}></Container>;
+  return (
+    <Container style={{ height }}>
+      <Logo height="4rem" />
+      <SocialMedia href="#" fillColor="#6f42c1">
+        <GitHubIcon />
+      </SocialMedia>
+
+      <SocialMedia href="#" fillColor="#0E76A8">
+        <LinkedInIcon />
+      </SocialMedia>
+
+      <SocialMedia href="#" fillColor="#3B5998">
+        <FacebookIcon />
+      </SocialMedia>
+    </Container>
+  );
 }
 
 type ContainerProps = {};
---
+++ client/src/components/ui/SocialMedia.tsx
@@ -0,0 +1,71 @@
+import IconButton from "@material-ui/core/IconButton";
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+
+type Props = ContainerProps & {
+  children: ReactElement;
+  href: string;
+};
+
+function SocialMedia({ children, href, fillColor }: Props): ReactElement {
+  return (
+    <Container fillColor={fillColor}>
+      <a href={href}>{children}</a>
+      <Filter />
+    </Container>
+  );
+}
+
+type ContainerProps = {
+  fillColor: string;
+};
+const Container = styled.span<ContainerProps>`
+  display: inline-block;
+  background-color: #fff;
+  width: 2.5em;
+  height: 2.5em;
+  border-radius: ${(p) => p.theme.shape.borderRadius}px;
+  box-shadow: ${(p) => p.theme.shadows[1]};
+
+  position: relative;
+
+  svg {
+    fill: ${(p) => p.fillColor};
+    position: relative;
+    z-index: ${(p) => p.theme.zIndex.tooltip};
+  }
+
+  a {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  :hover {
+    svg {
+      fill: #fff;
+    }
+
+    div {
+      clip-path: circle(150% at 0% 0%);
+      background-color: ${(p) => p.fillColor};
+    }
+  }
+`;
+
+type FilterProps = {};
+const Filter = styled.div<FilterProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  clip-path: circle(0 at 0% 0%);
+
+  transition: all ${(p) => p.theme.transitions.easing.easeInOut} 400ms;
+`;
+
+export { SocialMedia };
